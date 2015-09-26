ab79fd1bc09f2906d42db8426f387ac894b7d1d9
Guillermo Peralta <gperaltascura@gmail.com>
2015-09-26 19:15:02 -0400

    Se agrega función que devuelve coordenadas de los movimientos posibles

--- reversijs/scripts/reversi.js
+++ reversijs/scripts/reversi.js
@@ -30,12 +30,36 @@ function colocarFicha(c) {
     jugadorActual = jugadorActual === 1 ? 2 : 1;
     console.log(`Jugador ${jugadorActual}`);
     $("#labeljugador").text(`Jugador ${jugadorActual}`);
-    console.log(mainboard.state_vector);
     $("#puntaje1").text(cantFichas(mainboard, 1));
     $("#puntaje2").text(cantFichas(mainboard, 2));
     console.log("Total: " + cantFichas(mainboard));
     console.log("Evaluacion: " + evaluar(mainboard, 1));
+    console.log("Posibles movimientos: ");
+    var mov = posiblesMovimientos(mainboard, jugadorActual);
+    for(var i = 0; i < mov.length; i++) {
+      console.log(`[${mov[i][0]}, ${mov[i][1]}]`);
+    }
   }
+}
+//devuelve un array con las coordenadas [x, y] de los movimientos Posibles
+//para el jugador dado:
+//Ej.: de la respuesta: [4, 5], [3, 2], [5, 6]
+function posiblesMovimientos(tablero, jugador) {
+  var respuesta = [];
+  var jugActual = jugador === 1 ? -1 : 1;
+
+  for(var i=0; i<8; i++) {
+    for(var j=0; j<8; j++) {
+      var casillaActual = tablero.state_vector[i + 8 * j];
+      if(casillaActual != 0) continue;
+      var fichasComidas = tablero.allflips([i, j], jugActual);
+      if(fichasComidas.length === 0 ) {
+        continue;
+      }
+      respuesta.push([i, j]);
+    }
+  }
+  return respuesta;
 }
 
 function evaluar(tablero, jugadorIA) {
