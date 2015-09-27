7ca216dfbc9296cc6615c4b657637e2806c6bf08
Guillermo Peralta <gperaltascura@gmail.com>
2015-09-27 16:12:51 -0400

    Limpieza de codigo

--- reversijs/scripts/gui.js
+++ reversijs/scripts/gui.js
@@ -34,37 +34,5 @@ function drawall(board) {
   $('.rsquare').each(function() {
     $(this).find('img').attr('src', images[board.state(coords(this)) + 1]);
   });
-  drawtext(board);
   return true;
-}
-
-// Sets all the text (e.g., whose turn it is) based on the "board" object.
-function drawtext(board) {
-  function color(n) { return n == -1 ? 'black' : 'white'; }
-  var counts = board.countsquares();
-  $('#whitenum').html((counts[0] < 10 ? '&nbsp;' : '') + counts[0]);
-  $('#blacknum').html((counts[1] < 10 ? '&nbsp;' : '') + counts[1]);
-  $('#' + color(board.whosemove) + 'pointer').css({visibility: 'visible'})
-    .attr('title', color(board.whosemove) + ' to move');
-  $('#' + color(-board.whosemove) + 'pointer').css({visibility: 'hidden'})
-    .removeAttr('title');
-  $('#' + color(-board.computeris) + 'line')
-    .attr('title', 'you are ' + color(-board.computeris));
-  $('#' + color(board.computeris) + 'line')
-    .attr('title', 'click to play as ' + color(board.computeris));
-  $('#' + color(-board.computeris) + 'computer').css({visibility: 'hidden'});
-  $('#' + color(board.computeris) + 'computer').css({visibility: 'visible'});
-  if (counts[0] == 0 || counts[1] == 0 || counts[2] == 0) {
-    $('#pass').attr('value', 'Start');
-    $('#pass').removeAttr('disabled');
-  } else {
-    $('#pass').attr('value', 'Pass');
-    if (board.whosemove != board.computeris &&
-        counts[2] != 0 &&
-        board.bestmoves()[0].length == 0) {
-      $('#pass').removeAttr('disabled');
-    } else {
-      $('#pass').attr('disabled', true);
-    }
-  }
 }
